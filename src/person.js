c8e0fc8c03043c0408182363614e9b350e0ca640
Andrew Sova <andrew.sova@mabbly.com>
2019-01-17 18:36:56 +0200

    fix(js): Remove extra

--- src/person.js
+++
@@ -1,9 +0,0 @@
-export const isAdult = (age) => age>=18 ? true : false;
-
-export const canDrink = (age) => age>= 21 ? true : false;
-
-const isSenior = (age) => age>= 65;
-
-export default isSenior;
-
-// export {isAdult, canDrink};
