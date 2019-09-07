d9a756ad854cee327ccd1660e8e8ae3c4ac4bc78
Andrew Sova <andrew.sova@mabbly.com>
2019-09-07 14:33:00 +0300

    feat(withClass): Add HOC

--- burger-app/my-app-main/src/containers/App.js
+++ burger-app/my-app-main/src/containers/App.js
@@ -2,7 +2,8 @@ import React, { PureComponent } from "react";
 import classes from "./App.css";
 import Persons from "../components/Persons/Persons";
 import Cockpit from "../components/Cockpit/Cockpit";
-import WithClass from "../hoc/WithClass";
+import withClass from "../hoc/withClass";
+import Aux from "../hoc/Aux";
 
 class App extends PureComponent {
   constructor(props) {
@@ -83,7 +84,7 @@ class App extends PureComponent {
     }
 
     return (
-      <WithClass classes={classes.App}>
+      <Aux>
         <button
           onClick={() => {
             this.setState({ showCockpit: false });
@@ -100,9 +101,9 @@ class App extends PureComponent {
           />
         ) : null}
         {persons}
-      </WithClass>
+      </Aux>
     );
   }
 }
 
-export default App;
+export default withClass(App, classes.App);
