b7346dd3e31e67f6bf3de853b296d9b230f7376c
Andrew Sova <andrew.sova@mabbly.com>
2020-02-01 20:46:56 +0200

    feat(App): Create el Component

--- react-apps/todo/src/index.js
+++ react-apps/todo/src/index.js
@@ -1,1 +1,33 @@
-window.alert("Yo buddy!!!");
+import React from "react";
+import ReactDOM from "react-dom";
+
+const AppHeader = props => {
+  return <h1>{props.title}</h1>;
+};
+
+const SearchPanel = props => {
+  return <input type="text" placeholder={props.placeholder} />;
+};
+
+const TodoListItem = props => {
+  return <li>{props.content}</li>;
+};
+
+const TodoList = () => {
+  return (
+    <ul>
+      <TodoListItem content="Lear React" />
+      <TodoListItem content="Build Awesome App" />
+    </ul>
+  );
+};
+
+const el = (
+  <div>
+    <AppHeader title="My Todo list" />
+    <SearchPanel placeholder="Search..." />
+    <TodoList />
+  </div>
+);
+
+ReactDOM.render(el, document.getElementById("root"));
