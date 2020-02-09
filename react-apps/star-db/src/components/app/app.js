062bd5ff98d59046569ef23121406b36b4764e5e
Andrew Sova <andrew.sova@mabbly.com>
2020-02-10 00:20:38 +0200

    feat(Star DB): Add componentDidCatch

--- react-apps/star-db/src/components/app/app.js
+++ react-apps/star-db/src/components/app/app.js
@@ -3,13 +3,15 @@ import Header from '../header';
 import ItemList from '../item-list';
 import PersonDetails from '../person-details';
 import RandomPlanet from '../random-planet';
+import ErrorIndicator from "../error-indicator";
 
 import './app.css';
 
 export default class App extends Component {
 
     state = {
-        selectedPerson: 4
+        selectedPerson: 4,
+        hasError: false
     };
 
     onPersonSelected = (id) => {
@@ -18,7 +20,16 @@ export default class App extends Component {
         });
     };
 
+    componentDidCatch() {
+        this.setState({ hasError: true })
+    }
+
     render() {
+
+        if(this.state.hasError) {
+            return <ErrorIndicator />
+        }
+
         return (
             <div className="wrapper">
                 <Header />
