fd5b4272c9eccfa6e16b2dfc611a922640ba5f7b
Andrew Sova <andrew.sova@mabbly.com>
2020-02-13 20:23:42 +0200

    feat(Star DB): Transform props

--- react-apps/star-db/src/components/sw-component/item-lists.js
+++ react-apps/star-db/src/components/sw-component/item-lists.js
@@ -1,15 +1,7 @@
 import React from 'react';
 import ItemList from '../item-list';
-import { withData } from '../hoc-helpers';
-import SwapiService from '../../services/swapi-service';
-
-const swapiService = new SwapiService();
-
-const {
-    getAllPeople,
-    getAllPlanets,
-    getAllStarships
-} = swapiService;
+import { withData, withSwapiService } from '../hoc-helpers';
+//import swapi
 
 const withChildFunction = (Wrapped, fn) => {
     return ((props) => {
@@ -24,19 +16,43 @@ const withChildFunction = (Wrapped, fn) => {
 const renderName = ({ name }) => <span>{ name }</span>;
 const renderModelAndName = ({ name, model }) => <span>{ name } ({ model })</span>;
 
-const PersonList = withData(
-    withChildFunction(ItemList, renderName),
-    getAllPeople
+const mapPersonMethodsToProps = (swapiService) => {
+    return {
+        getData: swapiService.getAllPeople
+    };
+};
+
+const mapPlanetMethodsToProps = (swapiService) => {
+    return {
+        getData: swapiService.getAllPlanets
+    };
+};
+
+const mapStarshipMethodsToProps = (swapiService) => {
+    return {
+        getData: swapiService.getAllStarships
+    };
+};
+
+const PersonList = withSwapiService(
+    withData(
+        withChildFunction(ItemList, renderName)
+    ),
+    mapPersonMethodsToProps
 );
 
-const PlanetList = withData(
-    withChildFunction(ItemList, renderName),
-    getAllPlanets
+const PlanetList = withSwapiService(
+    withData(
+        withChildFunction(ItemList, renderName)
+    ),
+    mapPlanetMethodsToProps
 );
 
-const StarshipList = withData(
-    withChildFunction(ItemList, renderModelAndName),
-    getAllStarships
+const StarshipList = withSwapiService(
+    withData(
+        withChildFunction(ItemList, renderModelAndName)
+    ),
+    mapStarshipMethodsToProps
 );
 
 export {
