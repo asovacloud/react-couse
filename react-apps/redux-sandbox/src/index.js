6a1fc6b3b2c7fbc6c3319b5b6a5b60795993fe2b
Andrew Sova <andrew.sova@mabbly.com>
2020-02-26 21:03:27 +0200

    feat(redux sandbox): Add visual part

--- react-apps/redux-sandbox/src/index.js
+++ react-apps/redux-sandbox/src/index.js
@@ -3,8 +3,14 @@ import { createStore } from 'redux';
 const reducer = (state = 0, action) => {
 
   switch (action.type) {
+    case 'RND':
+      return state + action.payload;
+
     case 'INC':
       return state + 1;
+
+    case 'DEC':
+      return state - 1;
 
     default:
       return state;
@@ -13,10 +19,26 @@ const reducer = (state = 0, action) => {
 };
 
 const store = createStore(reducer);
-console.log(store.getState());
-store.subscribe(() => {
-  console.log(store.getState());
+
+document.getElementById('inc').addEventListener('click', () => {
+  store.dispatch({type: 'INC'})
 });
 
-store.dispatch({type: 'INC'});
-store.dispatch({type: 'INC'});
+document.getElementById('dec').addEventListener('click', () => {
+
+  store.dispatch({ type: 'DEC' })
+});
+
+document.getElementById('rnd').addEventListener('click', () => {
+  const payload = Math.floor(Math.random()*10);
+  store.dispatch({
+    type: 'RND',
+    payload
+  })
+});
+
+const update = () => {
+  document.getElementById('counter').innerHTML = store.getState();
+};
+
+store.subscribe(update);
