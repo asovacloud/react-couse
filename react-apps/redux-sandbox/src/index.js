9709f3a1f6e999e6528e4b50e62d06dc7bb05db9
Andrew Sova <andrew.sova@mabbly.com>
2020-02-26 20:27:11 +0200

    feat(Redux Sandbox): Add dispatch

---
+++ react-apps/redux-sandbox/src/index.js
@@ -0,0 +1,22 @@
+import { createStore } from 'redux';
+
+const reducer = (state = 0, action) => {
+
+  switch (action.type) {
+    case 'INC':
+      return state + 1;
+
+    default:
+      return state;
+  }
+
+};
+
+const store = createStore(reducer);
+console.log(store.getState());
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+store.dispatch({type: 'INC'});
+store.dispatch({type: 'INC'});
