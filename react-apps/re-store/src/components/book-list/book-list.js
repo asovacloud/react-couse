f663eb9e422c32b7b8dd0c0b1612c219b3f71d0b
Andrew Sova <andrew.sova@mabbly.com>
2020-04-03 11:30:50 +0300

    feat(Re Store): Add compose

--- react-apps/re-store/src/components/book-list/book-list.js
+++ react-apps/re-store/src/components/book-list/book-list.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
-import './book-list.css';
+import BookListItem from '../book-list-item';
 import { connect } from 'react-redux';
 
-import BookListItem from '../book-list-item';
+import { booksLoaded } from '../../actions';
+import { withBookstoreService } from '../hoc';
+import { compose } from '../../utils';
+
+import './book-list.css';
 
 class BookList extends Component {
+
+  componentDidMount() {
+    const { bookstoreService } = this.props;
+    const data = bookstoreService.getBooks();
+    
+    this.props.booksLoaded(data);
+  }
 
   render() {
     const { books } = this.props;
@@ -27,4 +38,11 @@ const mapStateToProps = ({ books }) => {
   return { books };
 };
 
-export default connect(mapStateToProps)(BookList);
+const mapDispatchToProps = {
+  booksLoaded
+};
+
+export default compose(
+  withBookstoreService(),
+  connect(mapStateToProps, mapDispatchToProps)
+)(BookList);
--- react-apps/re-store/src/index.js
+++ react-apps/re-store/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import App from './components/app';
 import ErrorBoundary from './components/error-boundary';
