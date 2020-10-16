1159c2f362d34e65bb18f5140206fab308e5e926
Andrew Sova <andrii.sova@tbwa-x.com>
2020-10-16 21:02:05 +0300

    Create Reducer

--- redux-sandbox/src/index.js
+++ redux-sandbox/src/index.js
@@ -1,1 +1,23 @@
-console.log('yo yo yo');
+const initialState = 0;
+
+const reducer = (state = initialState, action) => {
+
+  switch (action.type) {
+    case 'INC':
+      return state + 1;
+    default:
+      return state;
+  }
+
+};
+
+let state = reducer(undefined, {});
+
+state = reducer(state, { type: 'INC' });
+console.log(state);
+
+state = reducer(state, { type: 'INC' });
+console.log(state);
+
+state = reducer(state, { type: 'INC' });
+console.log(state);
