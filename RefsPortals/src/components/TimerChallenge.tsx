9e27004a33813b2a75ef6e14500d5932aa1dbcde
Andrii Sova <asovacloud@gmail.com>
2025-01-05 20:12:10 +0100

    Using refs more the DOM connection

--- RefsPortals/src/components/TimerChallenge.tsx
+++ RefsPortals/src/components/TimerChallenge.tsx
@@ -1,14 +1,38 @@
+import { useState, useRef } from 'react';
+
 const TimerChallenge = ({ title, targetTime }) => {
+  const timer = useRef(null);
+
+  const [timerStarted, setTimerStarted] = useState(false);
+  const [timerExpired, setTimerExpired] = useState(false);
+
+  const handleStart = () => {
+    timer.current = setTimeout(() => {
+      setTimerExpired(true);
+    }, targetTime * 1000);
+
+    setTimerStarted(true);
+  };
+
+  const handleStop = () => {
+    clearTimeout(timer.current);
+  };
+
   return (
     <section className="challenge">
       <h2>{title}</h2>
+      {timerExpired && <p>You lost!</p>}
       <p className="challenge-time">{`${targetTime} second${
         targetTime > 1 ? 's' : ''
       }`}</p>
       <p>
-        <button>Start challenge</button>
+        <button onClick={timerStarted ? handleStop : handleStart}>
+          {timerStarted ? 'Stop' : 'Start'} challenge
+        </button>
       </p>
-      <p className="">Time is running... / Timer inactive</p>
+      <p className={timerStarted ? 'active' : ''}>
+        {timerStarted ? 'Time is running...' : 'Timer inactive'}
+      </p>
     </section>
   );
 };
