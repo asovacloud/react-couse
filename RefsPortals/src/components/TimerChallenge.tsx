1246d72a88ddb26154ce37844d0a64fcd4192315
Andrii Sova <asovacloud@gmail.com>
2025-01-06 11:19:27 +0100

    Add a Result Modal component

--- RefsPortals/src/components/TimerChallenge.tsx
+++ RefsPortals/src/components/TimerChallenge.tsx
@@ -1,7 +1,9 @@
 import { useState, useRef } from 'react';
+import ResultModal from './ResultModal';
 
 const TimerChallenge = ({ title, targetTime }) => {
   const timer = useRef(null);
+  const dialog = useRef(null);
 
   const [timerStarted, setTimerStarted] = useState(false);
   const [timerExpired, setTimerExpired] = useState(false);
@@ -9,6 +11,7 @@ const TimerChallenge = ({ title, targetTime }) => {
   const handleStart = () => {
     timer.current = setTimeout(() => {
       setTimerExpired(true);
+      dialog.current.showModal();
     }, targetTime * 1000);
 
     setTimerStarted(true);
@@ -19,21 +22,24 @@ const TimerChallenge = ({ title, targetTime }) => {
   };
 
   return (
-    <section className="challenge">
-      <h2>{title}</h2>
-      {timerExpired && <p>You lost!</p>}
-      <p className="challenge-time">{`${targetTime} second${
-        targetTime > 1 ? 's' : ''
-      }`}</p>
-      <p>
-        <button onClick={timerStarted ? handleStop : handleStart}>
-          {timerStarted ? 'Stop' : 'Start'} challenge
-        </button>
-      </p>
-      <p className={timerStarted ? 'active' : ''}>
-        {timerStarted ? 'Time is running...' : 'Timer inactive'}
-      </p>
-    </section>
+    <>
+      <ResultModal ref={dialog} targetTime={targetTime} result="lost" />
+      <section className="challenge">
+        <h2>{title}</h2>
+        {timerExpired && <p>You lost 123!</p>}
+        <p className="challenge-time">{`${targetTime} second${
+          targetTime > 1 ? 's' : ''
+        }`}</p>
+        <p>
+          <button onClick={timerStarted ? handleStop : handleStart}>
+            {timerStarted ? 'Stop' : 'Start'} challenge
+          </button>
+        </p>
+        <p className={timerStarted ? 'active' : ''}>
+          {timerStarted ? 'Time is running...' : 'Timer inactive'}
+        </p>
+      </section>
+    </>
   );
 };
 
