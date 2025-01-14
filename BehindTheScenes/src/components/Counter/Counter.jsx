44ee91f49a45bde814c3aacf857b5517c3ec1e09
Andrii Sova <asovacloud@gmail.com>
2025-01-14 11:23:45 +0100

    Add a useCallback for functions to prevent extra rerender

--- BehindTheScenes/src/components/Counter/Counter.jsx
+++ BehindTheScenes/src/components/Counter/Counter.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { memo, useState, useCallback, useMemo } from 'react';
 
 import IconButton from '../UI/IconButton.jsx';
 import MinusIcon from '../UI/Icons/MinusIcon.jsx';
@@ -7,11 +7,7 @@ import CounterOutput from './CounterOutput.jsx';
 import { log } from '../../log.js';
 
 function isPrime(number) {
-  log(
-    'Calculating if is prime number',
-    2,
-    'other'
-  );
+  log('Calculating if is prime number', 2, 'other');
   if (number <= 1) {
     return false;
   }
@@ -27,19 +23,21 @@ function isPrime(number) {
   return true;
 }
 
-export default function Counter({ initialCount }) {
+const Counter = memo(function Counter({ initialCount }) {
   log('<Counter /> rendered', 1);
-  const initialCountIsPrime = isPrime(initialCount);
+  const initialCountIsPrime = useMemo(() => {
+    isPrime(initialCount);
+  }, [initialCount]);
 
   const [counter, setCounter] = useState(initialCount);
 
-  function handleDecrement() {
+  const handleDecrement = useCallback(function handleDecrement() {
     setCounter((prevCounter) => prevCounter - 1);
-  }
+  }, []);
 
-  function handleIncrement() {
+  const handleIncrement = useCallback(function handleIncrement() {
     setCounter((prevCounter) => prevCounter + 1);
-  }
+  }, []);
 
   return (
     <section className="counter">
@@ -58,4 +56,6 @@ export default function Counter({ initialCount }) {
       </p>
     </section>
   );
-}
+});
+
+export default Counter;
